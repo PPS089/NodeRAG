d530df8bedb4657910d50756c3fa7d906c97daa3
PPS089 <1809688953@qq.com>
2026-05-30 19:37:17 +0800

    导出所有模块

--- nodes/retrieval/__init__.py
+++ nodes/retrieval/__init__.py
@@ -1,1 +1,44 @@
+from nodes.retrieval.AutoMerger import (
+    auto_merge_hits,
+    integrate_auto_merge_to_retrieval,
+    merge_to_parent_level,
+    load_auto_merge_config,
+)
 
+from nodes.retrieval.GradeAndRewrite import (
+    grade_documents,
+    grade_retrieval_result,
+    grade_and_rewrite_loop,
+    rewrite_question,
+    choose_rewrite_strategy,
+)
+
+from nodes.retrieval.BM25State import (
+    BM25StateManager,
+    get_bm25_state_manager,
+    get_bm25_state_manager as bm25_state,
+)
+
+from nodes.retrieval.ChromaRetriever import retrieve
+
+from nodes.retrieval.bm25_recall import bm25_recall
+
+from nodes.retrieval.retrieval_config import (
+    DEFAULT_PER_QUERY_K,
+    DEFAULT_TOP_K,
+    DEFAULT_BM25_K,
+    DEFAULT_RERANK_POOL_SIZE,
+    DEFAULT_MMR_LAMBDA,
+)
+
+from nodes.retrieval.retrieval_utils import (
+    merge_hits,
+    unique_keep_order,
+    tokenize_for_search,
+)
+
+from nodes.retrieval.filters import build_where_filter
+
+from nodes.retrieval.context_expander import expand_hit_context
+
+from nodes.retrieval.chroma_store import get_collection, vector_recall
